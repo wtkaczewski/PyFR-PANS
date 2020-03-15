ea072481b55186d65447f1149076c92194957f40
tarikdzanic <tarikdzanic96@gmail.com>
2020-03-15 15:34:12 -0400

    Update vtk.py

--- pyfr/writers/vtk.py
+++ pyfr/writers/vtk.py
@@ -367,7 +367,8 @@ class VTKWriter(BaseWriter):
             norms = defaultdict(list)
 
             for etype, eidx, fidx, flags in self.mesh[bc].astype('U4,i4,i1,i1'):
-                eles = intg.system.ele_map[etype]
+                eles = self.elementscls.ele_map[etype]
+                print(self.elementscls.__dict__())
 
                 if (etype, fidx) not in m0:
                     facefpts = eles.basis.facefpts[fidx]
